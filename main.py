bd6faf176098794e41d3556ff3afd9ac7764dfe7
Himanshu <21himu@gmail.com>
2026-01-03 15:28:07 +0530

    Moving awa from pydantic formatting of output to with_structured_output

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from dotenv import load_dotenv
 from langchain_classic import hub
 from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
-from langchain_core.output_parsers.pydantic import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
-from langchain_core.runnables import Runnable, RunnableLambda
+from langchain_core.runnables import RunnableLambda
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
@@ -14,12 +13,13 @@ from schemas import AgentResponse
 load_dotenv()
 tools = [TavilySearch()]
 llm = ChatOpenAI(temperature=0, model="deepseek-chat")
+"""creating another instance of structured llm which is only responsible to """
+structured_llm = llm.with_structured_output(AgentResponse)
 react_prompt = hub.pull("hwchase17/react")
-output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
 react_prompt_with_format_instructions = PromptTemplate(
     input_variables=["input", "agent_scratchpad", "tool_names"],
     template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
-).partial(format_instructions=output_parser.get_format_instructions())
+).partial(format_instructions="")
 """ agent created from the create_react-agent is not actually the agent but we can consider as a chain of reasoning
 where this will give the input to LLM and then the llm will decide whom to call next be it llm again or any tools
 
@@ -37,9 +37,8 @@ AgentExecutor is the orchestrator who decides based on the llm output that what 
 """
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 extract_output = RunnableLambda(lambda x: x["output"])
-parse_output = RunnableLambda(lambda x: output_parser.parse(x))
 ##chain = agent_executor
-chain = agent_executor | extract_output | parse_output
+chain = agent_executor | extract_output | structured_llm
 
 
 def main():
